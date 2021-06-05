ed85dd404592aa31dfbc5c5059ef8c154ccaccab
Ivan <nighteliteace@gmail.com>
2021-06-05 09:43:13 +0200

    Stilovi.

--- ISAMRS_PROJEKAT/src/main/webapp/app/pregledApoteke.js
+++ ISAMRS_PROJEKAT/src/main/webapp/app/pregledApoteke.js
@@ -162,7 +162,8 @@ Vue.component("pregled-apoteke", {
 					  hideAfter : 5000,              // `false` to make it sticky or time in miliseconds to hide after
 					  stack : 5,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
 					  textAlign : 'left',            // Alignment of text i.e. left, right, center
-					  position : 'bottom-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+					  position : 'bottom-right',       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+					  font_size : 20,
 					});
 				}
 			})
